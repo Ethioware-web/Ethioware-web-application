08d915c8aa4f9a0aea4347b3c0460bbcc9efbfbc
naolethioware <lemesanaol93@ethioware.org>
2024-06-30 23:39:57 +0300

    Update script.js
    slowing the speed

--- Auto-Scrolling image array/script.js
+++ Auto-Scrolling image array/script.js
@@ -3,6 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const clone = wrapper.innerHTML;
     wrapper.innerHTML = clone+clone+clone;
     const contentWidth = wrapper.scrollWidth /2;
-    const animationDuration = contentWidth / 50; 
+    const animationDuration = contentWidth / 20; 
     wrapper.style.animation = `scroll ${animationDuration}s linear infinite`;
 });
