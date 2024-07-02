228e4d435cc628b98b9b1237be0108e33a70fbda
Inaswendem <inaswendem@ethioware.org>
2024-07-02 03:44:37 -0900

    Delete Auto-Scrolling image array directory

--- Auto-Scrolling image array/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const wrapper = document.getElementById('partner-wrapper');
-    const clone = wrapper.innerHTML;
-    wrapper.innerHTML = clone+clone+clone;
-    const contentWidth = wrapper.scrollWidth /2;
-    const animationDuration = contentWidth / 50; 
-    wrapper.style.animation = `scroll ${animationDuration}s linear infinite`;
-});
